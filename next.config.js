f2832f9ad76340bfb69f0d676986abb0c7ae41f0
akshay <thakuraks019@gmail.com>
2025-04-04 23:30:36 +0530

    final commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  eslint: {
+    // WARNING: This allows production builds to complete even if there are ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // WARNING: This allows production builds to complete even if there are TypeScript errors.
+    ignoreBuildErrors: true,
+  },
+};
