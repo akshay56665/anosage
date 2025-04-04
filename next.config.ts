b35c8b21a2b9f25f7a13775a17752299fdd14694
akshay <thakuraks019@gmail.com>
2025-04-04 16:28:49 +0530

    final commit

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  // Disables ESLint during production builds.
+  eslint: {
+    // Warning: This will allow production builds to complete even if there are ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
